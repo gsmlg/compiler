62ab2c0967b4bc4be554a019bac7040d509b746d
gsmlg <me@gsmlg.org>
2020-07-27 15:17:40 +0800

    add compiler test

--- __test__/compiler.test.js
+++ __test__/compiler.test.js
@@ -11,3 +11,10 @@ const input = `
 const output = compiler(input);
 
 console.log(output);
+
+describe('Compile lisp to C like language', () => {
+  it('list (add 3 5) should be', () => {
+    const source = '(add 3 5)';
+    expect(compiler(source)).toEqual('add(3, 5);');
+  });
+});
